47a0d9b6004a012d063695d03867d2af3376c723
Mike <vinertsev@gmail.com>
2017-10-24 19:50:11 +0700

    Update DAOPMTTOKEN.sol

--- src/ico/DAOPMTTOKEN.sol
+++ src/ico/DAOPMTTOKEN.sol
@@ -119,10 +119,10 @@ contract StandardToken is ERC20, SafeMath, Ownable{
    */
   function approve(address _spender, uint _value) public onlyPayloadSize(2) returns (bool success) {
     // To change the approve amount you first have to reduce the addresses`
-    //  allowance to zero by calling `approve(_spender, 0)` if it is not
+    //  approvals to zero by calling `approve(_spender, 0)` if it is not
     //  already 0 to mitigate the race condition described here:
     //  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-    assert((_value == 0) || (allowance[msg.sender][_spender] == 0));
+    assert((_value == 0) || (approvals[msg.sender][_spender] == 0));
     approvals[msg.sender][_spender] = _value;
     
     Approval(msg.sender, _spender, _value);
